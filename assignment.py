284dca31ea0d2298bdb328643c3cc8c749b87cac
dylancsu <dylan.dean@csuglobal.edu>
2025-02-02 16:05:53 -0700

    init

--- assignment.py
+++ assignment.py
@@ -1,1 +1,15 @@
 
+class Page:
+    def __init__(self, name, to, frm):
+        self.name=name
+        self.to=to
+        self.frm=frm
+
+main = Page("main", "list", "any")
+list = Page("list", "main, print", "main")
+print = Page("sync", "main", "list")
+
+pages = [main, list, print]
+
+for page, i in pages:
+    print(f"Page {i}: {page.name}. Reachable from {page.to}, reaches {page.frm}")
